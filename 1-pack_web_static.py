a39af2734bb5a5c7a5f1e034ae92d3a914375319
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-03-06 22:48:02 -0800

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -15,8 +15,8 @@ def do_pack():
                                                          dt.minute,
                                                          dt.second)
     if os.path.isdir("versions") is False:
-        if local(("mkdir -p versions").failed is True:
-                return None
+        if local("mkdir -p versions").failed is True:
+            return None
     if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
     return file
