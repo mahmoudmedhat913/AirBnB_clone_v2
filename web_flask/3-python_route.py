2fb07fe883f3edfd80271aa58576ac164943a8c1
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-03-23 05:21:16 -0700

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""web flask application"""
+from flask import Flask
+app = Flask('web_flask')
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_route():
+    """return string"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello_route2():
+    """return string"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def hello_route3(text):
+    """return text from html request"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/<text>')
+@app.route('/python/', defaults={'text': 'is cool'})
+def hello_route4(text):
+        """return text from html request"""
+            return 'Python {}'.format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
